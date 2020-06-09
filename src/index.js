76c4c19eb489a18e2ec345a4db7bb2ced781fc0d
nadda <nadda@mfec.co.th>
2020-06-09 13:45:49 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import logger from 'redux-logger';
+
+const store = createStore(rootReducer, applyMiddleware(logger))
+
+const MyApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
